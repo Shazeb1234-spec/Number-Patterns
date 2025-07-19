dcb1b87c6a1b5b0aad11edc7d3285de707f5b775
Shazeb Ramin Arafin <shazeb.arafin@gmail.com>
2025-07-19 11:22:30 +0400

    NumberPatternGenerator(V1)

---
+++ src/App.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
+
+public class App {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome To Number Pattern Generator :D");
+        System.out.println("Choose a Pattern To Generate");
+        System.out.println("1. Fibonacci Series");
+        System.out.println("2. Prime Numbers");
+        System.out.println("Enter Your Choice 1 or 2 :)");
+        int choice = scanner.nextInt();
+        
+
+    }
+}
